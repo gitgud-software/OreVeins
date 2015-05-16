a3898e37617b0d58ae86051ce50c05a6a174d54a
Gitgud Software <gitgud@tfwno.gf>
2015-05-15 20:36:55 -0400

    Commented out lines removing Vanilla Ore...
    For our purposes, this is unnecessary. This probably should've been configurable.

--- OreVeins/src/mcListenersAndPopulators/OreReplacer.java
+++ OreVeins/src/mcListenersAndPopulators/OreReplacer.java
@@ -22,27 +22,29 @@ public class OreReplacer extends BlockPopulator
 	{
 		//DebugLogger.console("deleting chunk at X:" + chunk.getX()+" Z:"+chunk.getZ());
 		Block block;
-		for (int x = 0; x < 16; x++)
-		{
-			for (int y = 1; y < 128; y++)
-			{
-				for (int z = 0; z < 16; z++)
-				{
-					block = chunk.getBlock(x, y, z);
-					if (block.getType().compareTo(Material.COAL_ORE)==0 
-							|| block.getType().compareTo(Material.IRON_ORE)==0 
-							|| block.getType().compareTo(Material.GOLD_ORE)==0 
-							|| block.getType().compareTo(Material.LAPIS_ORE)==0 
-							|| block.getType().compareTo(Material.REDSTONE_ORE)==0 
-							|| block.getType().compareTo(Material.DIAMOND_ORE)==0 
-							|| block.getType().compareTo(Material.EMERALD_ORE)==0)
-					{
-
-						chunk.getBlock(x, y, z).setType(Material.STONE);
-					}
-				}
-			}
-		}
+// These lines are made redundant with proper OrePlus configuration. Removed so that (hopefully) OreVeins plays nice with OrePlus. 
+//
+//		for (int x = 0; x < 16; x++)
+//		{
+//			for (int y = 1; y < 128; y++)
+//			{
+//				for (int z = 0; z < 16; z++)
+//				{
+//					block = chunk.getBlock(x, y, z);
+//					if (block.getType().compareTo(Material.COAL_ORE)==0 
+//							|| block.getType().compareTo(Material.IRON_ORE)==0 
+//							|| block.getType().compareTo(Material.GOLD_ORE)==0 
+//							|| block.getType().compareTo(Material.LAPIS_ORE)==0 
+//							|| block.getType().compareTo(Material.REDSTONE_ORE)==0 
+//							|| block.getType().compareTo(Material.DIAMOND_ORE)==0 
+//							|| block.getType().compareTo(Material.EMERALD_ORE)==0)
+//					{
+//
+//						chunk.getBlock(x, y, z).setType(Material.STONE);
+//					}
+//				}
+//			}
+//		}
 		TwoPoint theChunk = new TwoPoint(chunk.getX(),chunk.getZ(),true);
 		PointMapping.addToPopList(theChunk);
 	}
